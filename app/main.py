759ae3b3cbaa85378099dc68cd356f351330138f
Juan José Nicolás González Coloma <112291834+SushiDevice@users.noreply.github.com>
2025-04-10 13:51:08 +0000

    NEED API KEY AAAAAAAAAAAAAAA

--- app/main.py
+++ app/main.py
@@ -1,9 +1,19 @@
 from fastapi import FastAPI
 
+from routers import champions
+
 app = FastAPI()
 
 base_url = ""
 
+app.include_router(champions.router)
+
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "My last message",
+            "name": "Waosers"}
+
+@app.get("/lol")
+async def rt():
+    return {"message": "My last message",
+            "name": "Waosers"}
